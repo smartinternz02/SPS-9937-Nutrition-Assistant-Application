d10a9bfc42804e2162b13cbd7833e2653d4a49b3
IrfanDarood <83586264+IrfanDarood@users.noreply.github.com>
2021-05-20 15:52:28 +0530

    Update nutribaba.py

--- nutribaba.py
+++ nutribaba.py
@@ -6,6 +6,11 @@ import requests
 from ibm_watson import VisualRecognitionV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import hashlib
+import plotly
+import plotly.graph_objs as go
+
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'remotemysql.com'
@@ -37,7 +42,7 @@ def uploaddata():
         if pword == cpword:
             session["username"] = name
             pword = hashlib.md5(pword.encode())
-            pword=pword.hexdigest()
+            pword = pword.hexdigest()
             print(pword)
             cursor = mysql.connection.cursor()
             cursor.execute(
@@ -58,14 +63,14 @@ def authenticate():
         email = request.form['emailaddress']
         pword = request.form['pword']
         pword = hashlib.md5(pword.encode())
-        pword=pword.hexdigest()        
+        pword = pword.hexdigest()
 
         cursor = mysql.connection.cursor()
         cursor.execute('SELECT * FROM userdetails WHERE email= % s', (email,))
         mysql.connection.commit()
-        userexist=cursor.fetchone()
-        if userexist==None:
-            msg='User with this Email doesn\'t exist. Please Sign-up before Login'
+        userexist = cursor.fetchone()
+        if userexist == None:
+            msg = 'User with this Email doesn\'t exist. Please Sign-up before Login'
             return render_template('login.html', msg=msg)
         cursor.execute(
             'SELECT * FROM userdetails WHERE email= % s and pword = % s', (email, pword))
@@ -84,9 +89,11 @@ def authenticate():
 def trackfood():
     return render_template('trackfood.html')
 
+
 @app.route('/termsconditions')
 def termsconditions():
     return render_template('TermsConditions.html')
+
 
 @app.route('/logout')
 def logout():
@@ -117,22 +124,119 @@ def upload_img():
             fooditem = classes['images'][0]['classifiers'][0]['classes'][0]['class']
         except:
             return render_template('trackfood.html', msg=0)
+        if fooditem.lower() == 'non-food':
+            allnutrients = []
+            allnutrients.append(fooditem.upper())
+            allnutrients.append(0)
+            return render_template('trackfood.html', msg=allnutrients)
+        else:
+            # using usda api to get the nutrients of the food item
+            nutrients = requests.get('https://api.nal.usda.gov/fdc/v1/foods/search?query={}&pageSize={}&api_key={}'.format(
+                fooditem, '1', 'wNxP2RfBXrx3amU5HypuuEWUtSSgeRErZMcU5LFA'))
+            data = json.loads(nutrients.text)
+            allnutrients = {'meta': {}, 'Essentials': {}, 'Minerals': {
+            }, 'Vitamins': {}, 'Fattyacids': {}, 'Carotenoids': {}}
+            label = []
+            value = []
+            # print(json.dumps(data, indent=2))
+            n = len(data['foods'][0]['foodNutrients'])
+            allnutrients['meta']['fooditem'] = fooditem.upper()
+            allnutrients['meta']['nutrients'] = n
+            fattyacids = ['SFA', 'MUFA', 'PUFA']
+            allnutrients['Minerals']['total'] = 0
+            allnutrients['Vitamins']['total'] = 0
+            allnutrients['Fattyacids']['total'] = 0
+            allnutrients['Carotenoids']['total'] = 0
+            for i in range(0, n):
+                if(any([substring in (data['foods'][0]['foodNutrients'][i]['nutrientName']) for substring in fattyacids])):
+                    allnutrients['meta']['nutrients'] -= 1
+                    continue
+                no=int(data['foods'][0]['foodNutrients'][i]['nutrientNumber'])
+                if(no < 292 or no == 421 or no == 601):
+                    allnutrients['Essentials'][(data['foods'][0]['foodNutrients'][i]['nutrientName'])] = str(
+                        (data['foods'][0]['foodNutrients'][i]['value']))+" "+(data['foods'][0]['foodNutrients'][i]['unitName'])
+                    if (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'g':
+                        label.append(data['foods'][0]
+                                 ['foodNutrients'][i]['nutrientName'])
+                        value.append(data['foods'][0]['foodNutrients'][i]['value'])
+                    elif (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'mg':
+                        label.append(data['foods'][0]
+                                 ['foodNutrients'][i]['nutrientName'])
+                        value.append(
+                        (data['foods'][0]['foodNutrients'][i]['value'])*0.001)
+                    elif (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'ug':
+                        label.append(data['foods'][0]
+                                 ['foodNutrients'][i]['nutrientName'])
+                        value.append(
+                        (data['foods'][0]['foodNutrients'][i]['value'])*0.000001)
+                elif(no > 300 and no < 317):
+                    allnutrients['Minerals'][(data['foods'][0]['foodNutrients'][i]['nutrientName'])] = str(
+                        (data['foods'][0]['foodNutrients'][i]['value']))+" "+(data['foods'][0]['foodNutrients'][i]['unitName'])
 
-        # using usda api to get the nutrients of the food item
+                    if (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'g':
+                        allnutrients['Minerals']['total'] = (allnutrients['Minerals']['total'])+(data['foods'][0]['foodNutrients'][i]['value'])
+                    elif (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'mg':
+                        allnutrients['Minerals']['total'] = (allnutrients['Minerals']['total'])+(data['foods'][0]['foodNutrients'][i]['value'])*0.001
+                    elif (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'ug':
+                        allnutrients['Minerals']['total'] = (allnutrients['Minerals']['total'])+(data['foods'][0]['foodNutrients'][i]['value'])*0.000001
 
-        nutrients = requests.get('https://api.nal.usda.gov/fdc/v1/foods/search?query={}&pageSize={}&api_key={}'.format(
-            fooditem, '2', 'wNxP2RfBXrx3amU5HypuuEWUtSSgeRErZMcU5LFA'))
-        data = json.loads(nutrients.text)
-        allnutrients = []
-        n = len(data['foods'][0]['foodNutrients'])
-        allnutrients.append(fooditem.upper())
-        allnutrients.append(n*2)
-        for i in range(0, n):
-            allnutrients.append(
-                data['foods'][0]['foodNutrients'][i]['nutrientName'])
-            allnutrients.append(str(data['foods'][0]['foodNutrients'][i]['value']) +
-                                " "+data['foods'][0]['foodNutrients'][i]['unitName'])
-        return render_template('trackfood.html', msg=allnutrients)
+                elif(no >= 318 and no < 578 and no not in [321,322,334,337,338]):
+                    allnutrients['Vitamins'][(data['foods'][0]['foodNutrients'][i]['nutrientName'])] = str(
+                        (data['foods'][0]['foodNutrients'][i]['value']))+" "+(data['foods'][0]['foodNutrients'][i]['unitName'])
+
+                    if (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'g':
+                        allnutrients['Vitamins']['total'] = (allnutrients['Vitamins']['total'])+(data['foods'][0]['foodNutrients'][i]['value'])
+                    elif (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'mg':
+                        allnutrients['Vitamins']['total'] = (allnutrients['Vitamins']['total'])+(data['foods'][0]['foodNutrients'][i]['value'])*0.001
+                    elif (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'ug':
+                        allnutrients['Vitamins']['total'] = (allnutrients['Vitamins']['total'])+(data['foods'][0]['foodNutrients'][i]['value'])*0.000001
+
+                elif(no in [321,322,334,337,338]):
+                    allnutrients['Carotenoids'][(data['foods'][0]['foodNutrients'][i]['nutrientName'])] = str(
+                        (data['foods'][0]['foodNutrients'][i]['value']))+" "+(data['foods'][0]['foodNutrients'][i]['unitName'])
+                    
+                    if (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'g':
+                        allnutrients['Carotenoids']['total'] = (allnutrients['Carotenoids']['total'])+(data['foods'][0]['foodNutrients'][i]['value'])
+                    elif (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'mg':
+                        allnutrients['Carotenoids']['total'] = (allnutrients['Carotenoids']['total'])+(data['foods'][0]['foodNutrients'][i]['value'])*0.001
+                    elif (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'ug':
+                        allnutrients['Carotenoids']['total'] = (allnutrients['Carotenoids']['total'])+(data['foods'][0]['foodNutrients'][i]['value'])*0.000001
+
+                elif(no >= 602):
+                    allnutrients['Fattyacids'][(data['foods'][0]['foodNutrients'][i]['nutrientName'])] = str(
+                        (data['foods'][0]['foodNutrients'][i]['value']))+" "+(data['foods'][0]['foodNutrients'][i]['unitName'])
+                    if (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'g':
+                        allnutrients['Fattyacids']['total'] = (allnutrients['Fattyacids']['total'])+(data['foods'][0]['foodNutrients'][i]['value'])
+                    elif (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'mg':
+                        allnutrients['Fattyacids']['total'] = (allnutrients['Fattyacids']['total'])+((data['foods'][0]['foodNutrients'][i]['value'])*0.001)
+                    elif (data['foods'][0]['foodNutrients'][i]['unitName']).lower() == 'ug':
+                        allnutrients['Fattyacids']['total'] = (allnutrients['Fattyacids']['total'])+((data['foods'][0]['foodNutrients'][i]['value'])*0.000001)
+                    
+# the below code is to enter the sub groups of nutrients created above, into the pie chart label and values and also into the allnutrients dictionary 
+
+            label.append('Minerals')
+            label.append('Vitamins')
+            label.append('Carotenoids')
+            label.append('Fattyacids')
+            value.append(allnutrients['Minerals']['total'])
+            value.append(allnutrients['Vitamins']['total'])
+            value.append(allnutrients['Carotenoids']['total'])
+            value.append(allnutrients['Fattyacids']['total'])
+            allnutrients['Minerals']['total'] = str(round(((allnutrients['Minerals']['total'])*1000),1))+" "+"MG"
+            allnutrients['Vitamins']['total'] = str(round(((allnutrients['Vitamins']['total'])*1000),1))+" "+"MG"
+            allnutrients['Carotenoids']['total'] = str(round(((allnutrients['Carotenoids']['total'])*1000),1))+" "+"MG"
+            allnutrients['Fattyacids']['total'] = str(round(((allnutrients['Fattyacids']['total'])*1000),1))+" "+"MG"
+
+# cretion of the piechart using plotly go
+            data = [
+                go.Pie(
+                    labels=label,
+                    values=value
+                )
+            ]
+            graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+            return render_template('trackfood.html', msg=allnutrients, plot=graphJSON)
+
 
 @app.route('/mybmi')
 def mybmi():
@@ -142,6 +246,33 @@ def mybmi():
 @app.route('/nutrition')
 def nutrition():
     return render_template('nutrition.html')
+
+
+@app.route('/getchart')
+def getchart():
+    pie = create_plot()
+    return render_template('demo.html', plot=pie)
+
+
+def create_plot():
+
+    N = 40
+    x = np.linspace(0, 1, N)
+    y = np.random.randn(N)
+    # creating a sample dataframe
+    df = pd.DataFrame({'x': [3, 4, 2, 5], 'y': [3, 3, 3, 3]})
+
+    data = [
+        go.Pie(
+            labels=[3, 4, 2, 5],  # assign x as the dataframe column 'x'
+            values=[3, 3, 3, 3]
+        )
+    ]
+
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=8080)
